c16ddc54e9bc6a01b9ed7a1a38f61cbf5e27d0ae
dano210905 <dany.hurtac@gmail.com>
2025-05-13 21:18:12 -0500

    Update ManejoPila.java

--- Parqueadero/src/parqueadero/ManejoPila.java
+++ Parqueadero/src/parqueadero/ManejoPila.java
@@ -37,6 +37,7 @@ public class ManejoPila
         }//fin mientras
     }//fin devolver
     
+    
             
     /*metodo que retorna en una cadena el contenido de la pila sin perder los datos*/
     public String JuntarPila(Pila p1, Pila p2)
@@ -56,5 +57,137 @@ public class ManejoPila
         Devolver(p2,p1);
         return texto;
     }//fin juntar
+
     
+public boolean Buscar(Pila p1, Pila p2, String idb)
+{
+boolean sw=false;
+Object info;
+while(p1.IsEmpty()==false)
+{
+info=p1.Pop();
+if(((Servicios)info).getId().equalsIgnoreCase(idb))
+	sw=true;
+//fin si
+p2.Push(info);//para no peder dato
+}//fin mientras
+Devolver(p2,p1);
+return sw;
+}//fin
+
+public Pila EliminarDato(Pila p1, Pila p2, String ide)
+{
+Object info;
+if(Buscar(p1,p2,ide)==false)
+       JOptionPane.showMessageDialog(null,"Dato a eliminar no se encuentra en la pila");
+else
+{
+while(p1.IsEmpty()==false)
+{
+    info=p1.Pop();
+    if(!(((Servicios)info).getId().equalsIgnoreCase(ide)))
+       p2.Push(info);
+    //fin si
+}//fin mientras
+Devolver(p2,p1);
+JOptionPane.showMessageDialog(null,"Dato se elimina de la pila "+ide);
+}//Fin si
+return p1;
+}//Fin 
+
+
+public Object ConsultarDato(Pila p1, Pila p2, String idc)
+{
+Object info,datoc=null;
+if(Buscar(p1,p2,idc)==false)
+       JOptionPane.showMessageDialog(null,"Dato a consultar no se encuentra en la pila");
+else
+{
+while(p1.IsEmpty()==false)
+{
+    info=p1.Pop();
+    if(((Servicios)info).getId().equalsIgnoreCase(idc))
+      datoc=info;
+    //fin si
+    p2.Push(info);
+}//fin mientras
+Devolver(p2,p1);
+}//Fin si
+return datoc;
+}//Fin 
+
+public Pila ActualizarDatos(Pila p1, Pila p2, String idm)
+{
+Object  info;
+String Tser;
+double VlrSer;
+int op,resp;
+if(Buscar(p1,p2,idm)==false)
+        JOptionPane.showMessageDialog(null,"Dato a modificar no se encuentra en la pila");
+else
+{
+    while(p1.IsEmpty()==false)
+    {
+    info=p1.Pop();
+    if(((Servicios)info).getId().equalsIgnoreCase(idm))
+    {
+       Tser=((Servicios)info).getTipoServicio();
+       VlrSer=((Servicios)info).getValorServicio();	
+	do{	      
+	   op=Validaciones.LeerInt("menú actualización"+
+				   "Id= "+ idm+"\n"+
+                                   "1. Tipo Servicio= "+Tser+"\n"+
+                                   "2. Valor servicio= "+VlrSer+"\n"+
+                                   "3. Terminar la actualización");
+           switch(op)// En caso(op)	
+           {
+	    case 1: Tser=Validaciones.LeerTipoServicios();
+                    break; 
+	    case 2: VlrSer=Validaciones.LeerDouble("valor servicio: ");
+                    break;
+           }//Fin caso
+        }while(op<3);
+        resp=JOptionPane.showConfirmDialog(null,"Grabar los cambios?","Actualiza",JOptionPane.YES_OPTION);
+        if(resp==JOptionPane.YES_OPTION)
+        {
+            ((Servicios)info).setTipoServicio(Tser);
+            ((Servicios)info).setValorServicio(VlrSer);
+        }//Fin si
+    }//finsi
+    p2.Push(info);
+    }//fin mientras
+Devolver(p2,p1);
+}//fin si 
+return p1;
+}//fin
+
+
+    /*Metodo que desapila el ultimo dato ingresado*/
+public Object Desapilar(Pila p1)
+{
+    return p1.Pop();
+}
+    /*Metodo que desapila el primer dato ingresado */
+       
+public Object DesapilarPrimero(Pila p1, Pila p2)
+{
+ Devolver(p1,p2);
+ p2.Pop();
+ Devolver(p2,p1);
+ return p1;
+}
+
+
+public int ContarElementos (Pila p1, Pila p2)
+{
+    int cont = 0; //contador de retorno
+    while (p1.IsEmpty()== false)
+    {
+        p2.Push(p1.Pop());
+        cont++;
+    }
+   Devolver(p2, p1);
+   return cont;
+}
 }//fin clase manejo pila
+
